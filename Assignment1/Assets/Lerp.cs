f24193244f9fad997993054c57e3a52478045a57
rad-ical-bell <westiequeenrad@gmail.com>
2025-02-12 18:45:00 -0500

    Modification of Lerp.cs
    Changing the Lerp.cs file & updating Heart.png

--- Assignment1/Assets/Lerp.cs
+++ Assignment1/Assets/Lerp.cs
@@ -10,19 +10,19 @@ using UnityEngine;
 
 public class Lerp : MonoBehaviour
 {
-    GameObject[] spheres;
+    GameObject[] capsules;
     static int numSphere = 500; 
     float time = 0f;
     Vector3[] initPos;
     Vector3[] startPosition, endPosition;
     float lerpFraction; // Lerp point between 0~1
     float t;
+    float random; // random rotation
 
-    // Start is called before the first frame update
     void Start()
     {
         // Assign proper types and sizes to the variables.
-        spheres = new GameObject[numSphere];
+        capsules = new GameObject[numSphere];
         initPos = new Vector3[numSphere]; // Start positions
         startPosition = new Vector3[numSphere]; 
         endPosition = new Vector3[numSphere]; 
@@ -39,52 +39,45 @@ public class Lerp : MonoBehaviour
                         5f* (- Mathf.Cos(t) * Mathf.Cos(t) * Mathf.Cos(t) - Mathf.Cos(t) * Mathf.Cos(t) + 2 *Mathf.Cos(t)) + 3f,
                         10f + Mathf.Sin(time));
         }
-        // Let there be spheres..
+
         for (int i =0; i < numSphere; i++){
-            float r = 10f; // radius of the circle
-            // Draw primitive elements:
-            // https://docs.unity3d.com/6000.0/Documentation/ScriptReference/GameObject.CreatePrimitive.html
-            spheres[i] = GameObject.CreatePrimitive(PrimitiveType.Sphere); 
+            float r = 1.5f; // Modified radius of the capsules
+            
+            capsules[i] = GameObject.CreatePrimitive(PrimitiveType.Capsule); 
 
             // Position
             initPos[i] = startPosition[i];
-            spheres[i].transform.position = initPos[i];
+            capsules[i].transform.position = initPos[i];
+            //Created random rotation for each capsule
+            random = (float)Random.Range(0,360);
+            capsules[i].transform.rotation = Quaternion.Euler(0, 0, random);
 
             // Color
-            // Get the renderer of the spheres and assign colors.
-            Renderer sphereRenderer = spheres[i].GetComponent<Renderer>();
-            // HSV color space: https://en.wikipedia.org/wiki/HSL_and_HSV
+            Renderer sphereRenderer = capsules[i].GetComponent<Renderer>();
             float hue = (float)i / numSphere; // Hue cycles through 0 to 1
-            Color color = Color.HSVToRGB(hue, 1f, 1f); // Full saturation and brightness
+            Color color = Color.HSVToRGB(hue, 1f, 1f); 
             sphereRenderer.material.color = color;
         }
     }
 
-    // Update is called once per frame
     void Update()
     {
         // Measure Time 
-        time += Time.deltaTime; // Time.deltaTime = The interval in seconds from the last frame to the current one
-        // what to update over time?
+        time += Time.deltaTime; 
         for (int i =0; i < numSphere; i++){
-            // Lerp : Linearly interpolates between two points.
-            // https://docs.unity3d.com/6000.0/Documentation/ScriptReference/Vector3.Lerp.html
-            // Vector3.Lerp(startPosition, endPosition, lerpFraction)
             
             // lerpFraction variable defines the point between startPosition and endPosition (0~1)
             // let it oscillate over time using sin function
-            lerpFraction = Mathf.Sin(time) * 0.5f + 0.5f;
+            lerpFraction = Mathf.Sin(time) * .60f + 0.5f; //Modified the oscillation
 
             // Lerp logic. Update position       
             t = i* 2 * Mathf.PI / numSphere;
-            spheres[i].transform.position = Vector3.Lerp(startPosition[i], endPosition[i], lerpFraction);
-            // For now, start positions and end positions are fixed. But what if you change it over time?
-            // startPosition[i]; endPosition[i];
+            capsules[i].transform.position = Vector3.Lerp(startPosition[i], endPosition[i], lerpFraction);
 
             // Color Update over time
-            Renderer sphereRenderer = spheres[i].GetComponent<Renderer>();
+            Renderer sphereRenderer = capsules[i].GetComponent<Renderer>();
             float hue = (float)i / numSphere; // Hue cycles through 0 to 1
-            Color color = Color.HSVToRGB(Mathf.Abs(hue * Mathf.Sin(time)), Mathf.Cos(time), 2f + Mathf.Cos(time)); // Full saturation and brightness
+            Color color = Color.HSVToRGB(Mathf.Abs(hue * Mathf.Sin(time)), 0.75f, 2f + Mathf.Cos(time)); // Full saturation and brightness
             sphereRenderer.material.color = color;
         }
     }
